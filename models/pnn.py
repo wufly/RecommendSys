87c8a2b8a80fa3cb16641a61af8a98f6b50f006d
吴飞 <wufei@wufeideMacBook-Pro.local>
2021-10-20 18:03:40 +0800

    add pnn_inner

---
+++ models/pnn.py
@@ -0,0 +1,77 @@
+from utils import *
+
+
+class InnerProductLayer(Layer):
+
+    def __init__(self, d1, reg=1e-4, **kwargs):
+        """
+
+        :param d1: 第一层mlp的units个数
+        :param kwargs:
+        """
+        super(InnerProductLayer, self).__init__(**kwargs)
+        self.d1 = d1
+        self.reg = reg
+        self.Wp = None
+        self.b1 = None
+
+    def build(self, input_shape):
+        self.Wp = self.add_weight(
+            name="Wp",
+            shape=(self.d1, input_shape[1]),
+            initializer=tf.initializers.glorot_normal(),
+            regularizer=tf.keras.regularizers.l2(self.reg),
+            trainable=True
+        )
+        self.b1 = self.add_weight(
+            name="b1",
+            shape=(1, self.d1),
+            initializer=tf.initializers.glorot_normal(),
+            regularizer=tf.keras.regularizers.l2(self.reg),
+            trainable=True
+        )
+
+    def call(self, inputs, **kwargs):
+        """
+
+        :param inputs: shape:[None, n, m]
+        :param kwargs:
+        :return:
+        """
+
+        lp = []
+        for i in range(self.d1):
+            signal = K.reshape(self.Wp[i], (len(self.Wp[i]), 1)) * inputs
+            lp.append(0.5 * tf.reduce_sum(tf.pow(tf.reduce_sum(signal, axis=1), 2) - tf.reduce_sum(tf.pow(signal, 2), axis=1), axis=1, keepdims=True))
+        lp = Concatenate(axis=-1)(lp)
+        return lp # [None, d1]
+
+    def get_config(self):
+        config = super(InnerProductLayer, self).get_config()
+        config.update({'d1': self.d1, 'reg': self.reg})
+
+
+def pnn(inputs, columns_name, product_type='inner', hidden_unit=(128, 64, 32), activation='relu', seed=1024):
+
+    input_feature_columns = [feat_col for feat_col in inputs if feat_col.name in columns_name]
+    keras_input = build_keras_inputs(input_feature_columns)
+    input_list = list(keras_input.values())
+
+    embedding_dict = build_embedding_dict(input_feature_columns)
+    sparse_value_list, _ = input_from_feature_columns(keras_input, input_feature_columns, embedding_dict)
+    concat_embedding_values = Concatenate(axis=1)(sparse_value_list)
+    # if product_type == 'inner':
+    lp_output = InnerProductLayer(hidden_unit[0])(concat_embedding_values)
+    # lz part
+    lz_input = combine_dnn_inputs(sparse_value_list, [])
+    lz_output = Dense(hidden_unit[0])(lz_input)
+    inner_product_layer = tf.keras.activations.relu(Add()([lp_output, lz_output]))
+    output = 0
+    for i in range(1, len(hidden_unit)):
+        if i == len(hidden_unit) - 1:
+            output = Dense(hidden_unit[i], activation=activation)(inner_product_layer)
+        else:
+            inner_product_layer = Dense(hidden_unit[i], activation=activation)(inner_product_layer)
+
+    final_output = Dense(1, activation='sigmoid')(output)
+    return tf.keras.Model(input_list, final_output)
